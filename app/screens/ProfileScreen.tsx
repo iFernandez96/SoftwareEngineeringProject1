5ac08fe519e338828cca590b866402ecfed71c84
Israel Fernandez <israelfernandez96@gmail.com>
2025-02-16 16:10:42 -0800

    Udpated profile screen with bare bones modal

--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -1,10 +1,13 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, {useState} from 'react';
+import { View, Text, Button, StyleSheet, Pressable, Modal, Alert } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { getUserName } from '@/database/Database'
 import { useRouter } from "expo-router";
 
 export default function ProfileScreen() {
   const router = useRouter();
-
+  const [modalVisible, setModalVisible] = useState(false);
   const handleLogout = async () => {
     await AsyncStorage.removeItem("user"); // clear user session
     router.replace("/login"); // go to login
@@ -12,6 +15,35 @@ export default function ProfileScreen() {
 
   return (
     <View style={styles.container}>
+      <SafeAreaProvider>
+      <SafeAreaView style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <Pressable
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}>
+          <Text style={styles.textStyle}>Show Modal</Text>
+        </Pressable>
+      </SafeAreaView>
+    </SafeAreaProvider>
+
       <Text style={styles.text}>Your Profile</Text>
       <Button title="Sign Out" onPress={handleLogout} color="#ff4444" />
     </View>
@@ -19,6 +51,46 @@ export default function ProfileScreen() {
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
     alignItems: "center",
